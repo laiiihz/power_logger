407efd1db0d9a82e68c24fb450752e248471c4e9
laiiihz <laiiihz@outlook.com>
2021-02-19 14:01:31 +0800

    format code

--- lib/src/power_logger_view.dart
+++ lib/src/power_logger_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:power_logger/power_logger.dart';
 
-
 ///Logger view
 class PowerLoggerView extends StatefulWidget {
   PowerLoggerView({Key key}) : super(key: key);
--- lib/src/view/box_view.dart
+++ lib/src/view/box_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-  ///BoxView
+///BoxView
 class BoxView extends StatelessWidget {
   final Widget title;
   final Widget child;
